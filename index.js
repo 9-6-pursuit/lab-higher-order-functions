68385719e0e51a9fce7debfb67c9a3b3d7618b95
Phillip Andrew Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-02-16 18:17:00 -0500

    2 tests didn't pass

--- index.js
+++ index.js
@@ -14,6 +14,9 @@
  */
 function find(array, callback) {
   for (let element of array) {
+    if(callback(element)){
+      return element
+    }
     // Write your code here.
   }
 }
@@ -33,11 +36,16 @@ function find(array, callback) {
  *  //> []
  */
 function filter(array, callback) {
-  const result = [];
-  for (let element of array) {
-    // Write your code here.
+  let result = [];
+  for (const element of array) {
+    if (callback(element)){
+        result.push(element)
+    }
   }
   return result;
+}
+const returnFalseIfArrayIsEmpty = function(element) {
+  return result
 }
 
 /**
@@ -57,6 +65,7 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
+    result.push(callback(element))
     // Write your code here.
   }
   return result;
@@ -77,7 +86,7 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+    
   }
 }
 
